2ad8b86c1e30eebe84a7a5801c599a1c90654c4b
林树毅 <2031624910@qq.com>
2022-10-19 14:37:13 +0800

    提交

---
+++ record/src/main/java/com/extra/light/record/RecordApplication.java
@@ -0,0 +1,17 @@
+package com.extra.light.record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 创建于2022-10-19
+ * 用于测试相关服务与项目相关的连接。以及新知识的实际应用测试
+ *
+ * @author 林树毅
+ */
+@SpringBootApplication
+public class RecordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecordApplication.class);
+    }
+}
